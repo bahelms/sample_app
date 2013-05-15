25fc9fa626cdecb4368154606a87713bac2e51ea
Jimbonk <jimbonkgit@gmail.com>
2013-05-15 14:46:49 -0400

    Added application_helper_spec

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
 describe "User pages" do
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   subject { page }
 
   describe "signup page" do
     before { visit signup_path }
 
-    it { should have_content('Sign up') }
-    it { should have_title("#{base_title} | Sign up")}
+    it { should have_content('Sign Up') }
+    it { should have_title(full_title('Sign Up')) }
   end
 end
