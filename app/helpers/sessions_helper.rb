df713302b9713a7a65ca9b8752c44e4aca3bae96
Jimbonk <jimbonkgit@gmail.com>
2013-05-29 06:31:11 -0400

    Heroku fix

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,7 +13,9 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= User.find_by(remember_token: cookies[:remember_token])
+    if cookies[:remember_token]
+      @current_user ||= User.find_by(remember_token: cookies[:remember_token])
+    end
   end
 
   def sign_out
